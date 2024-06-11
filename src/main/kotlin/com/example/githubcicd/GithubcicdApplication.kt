dfb756d6c039d9b8ded3c798c831449bb155c6ad
harshhffc <harshit.chauhan@homefirstindia.com>
2024-06-11 14:52:41 +0530

    First commit

---
+++ src/main/kotlin/com/example/githubcicd/GithubcicdApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubcicd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubcicdApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubcicdApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/githubcicd/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.example.githubcicd
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(GithubcicdApplication::class.java)
+	}
+
+}
